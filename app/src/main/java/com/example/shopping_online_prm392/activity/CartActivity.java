6d908095338948ed144cad49ab997818777fd826
DiorEtMoi <110081209+DiorEtMoi@users.noreply.github.com>
2023-11-03 01:20:58 +0700

    udpate

--- app/src/main/java/com/example/shopping_online_prm392/activity/CartActivity.java
+++ app/src/main/java/com/example/shopping_online_prm392/activity/CartActivity.java
@@ -11,6 +11,7 @@ import com.example.shopping_online_prm392.R;
 import com.example.shopping_online_prm392.adapter.CartAdapter;
 import com.example.shopping_online_prm392.model.Cart;
 import com.example.shopping_online_prm392.model.Product;
+import com.example.shopping_online_prm392.utils.ProductFireBase;
 import com.example.shopping_online_prm392.utils.Utils;
 
 import java.util.ArrayList;
@@ -18,6 +19,7 @@ import java.util.List;
 
 public class CartActivity extends AppCompatActivity {
     private  List<Cart> cartProducts;
+    private ProductFireBase productFirebase;
     private RecyclerView rcvCart;
     private CartAdapter cartAdapter;
 
@@ -25,28 +27,24 @@ public class CartActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cart_product);
+        BindingData();
         BindingView();
     }
     private void BindingView(){
-        cartProducts = new ArrayList<>();
-        Product p = createProduct();
-        Cart c = new Cart(p,1,p.getPrice());
-        Cart c1 = new Cart(p,1,p.getPrice());
-        cartProducts.add(c);
-        cartProducts.add(c1);
         rcvCart = findViewById(R.id.cart_product);
         cartAdapter = new CartAdapter(cartProducts);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         rcvCart.setLayoutManager(linearLayoutManager);
         rcvCart.setAdapter(cartAdapter);
     }
-    private Product createProduct(){
-        Product p = new Product();
-        p.setName("Product 1");
-        p.setQuantity(10);
-        p.setId("1");
-        p.setPrice(10);
-        p.setDescription("123123123");
-        return p;
+    private  void BindingData(){
+        cartProducts = new ArrayList<>();
+        productFirebase = new ProductFireBase();
+        List<Product> products = productFirebase.GetAllProduct(this);
+        for (Product product : products){
+            Cart c = new Cart(product,1,1);
+            cartProducts.add(c);
+        }
     }
+
 }
