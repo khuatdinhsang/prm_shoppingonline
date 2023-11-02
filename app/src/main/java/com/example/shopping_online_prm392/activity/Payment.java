0720fc2193663538414b8b95ff0d4c114d1ff287
Vuong <vuongnguyenvan282@gmail.com>
2023-11-02 22:59:12 +0700

    Update

---
+++ app/src/main/java/com/example/shopping_online_prm392/activity/Payment.java
@@ -0,0 +1,16 @@
+package com.example.shopping_online_prm392.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.shopping_online_prm392.R;
+
+public class Payment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+    }
+}
