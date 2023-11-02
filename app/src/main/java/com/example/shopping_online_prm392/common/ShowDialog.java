a8855fdfefe4ab4f727a01fd6d6a5ad88accbccc
khuatdinhsang <sangkdhe161711@fpt.edu.vn>
2023-11-02 12:09:43 +0700

    done logout

--- app/src/main/java/com/example/shopping_online_prm392/common/ShowDialog.java
+++ app/src/main/java/com/example/shopping_online_prm392/common/ShowDialog.java
@@ -1,5 +1,50 @@
 package com.example.shopping_online_prm392.common;
 
-public class ShowDialog {
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.widget.Toast;
 
+public class ShowDialog {
+    public interface ConfirmationDialogListener {
+        void onYesClicked();
+        void onNoClicked();
+    }
+    public static void showConfirmationDialog(Context context, String message,
+                                              final ConfirmationDialogListener listener) {
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+        alertDialogBuilder.setMessage(message);
+        alertDialogBuilder.setPositiveButton("Yes",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (listener != null) {
+                            listener.onYesClicked();
+                        }
+                    }
+                });
+
+        alertDialogBuilder.setNegativeButton("No",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        listener.onNoClicked();
+                    }
+                });
+        alertDialogBuilder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                listener.onNoClicked();
+            }
+        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    public static void showMessage(Context context, String message){
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
 }
